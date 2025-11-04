92e38ba3817e14d83e0965472b91d2fbc78b3096
misha1391 <misha.martynov2009.1@gmail.com>
2025-11-04 22:24:03 +0300

    for homework 2025.10.30(not completed)(client uses json files)

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import requests
+import json
 
 # def hi():
 #     print("Hello world")
@@ -9,33 +10,45 @@ import requests
 
 URL = "http://127.0.0.1:8000"
 
-def get_grades():
+def load_data(fileName: str):
+    try:
+        with open(fileName, "r") as f:
+            return json.load(f)
+    except Exception as e:
+        return []
+def save_data(fileName: str, data):
+    with open(fileName, "w") as f:
+        return json.dump(data, f, indent=2)
+def get_grades(fileName: str):
     req = requests.get(f"{URL}/grades")
+    save_data(fileName, req.json())
     print(req.json())
 def get_by_id(grade_id):
     res = requests.get(f"{URL}/grades/{grade_id}")
     print(res.json())
-def add_grade(name: str, subject: str, grade: int, date: str, teacher: str):
+def add_grade(fileName: str):
+    grade = load_data(fileName)[0]
     data = {
-        "name": name,
-        "subject": subject,
-        "grade": grade,
-        "date": date,
-        "teacher": teacher
+        "name": grade["name"],
+        "subject": grade["subject"],
+        "grade": grade["grade"],
+        "date": grade["date"],
+        "teacher": grade["teacher"]
     }
     res = requests.post(f"{URL}/grades", json=data)
     print(res.json())
 def delete_grade(grade_id):
     res = requests.delete(f"{URL}/grades/{grade_id}")
     print(res.json())
-def change_grade(grade_id: int, name: str, subject: str, grade: int, date: str, teacher: str):
+def change_grade(fileName: str):
+    grade = load_data(fileName)[0]
     data = {
-        "id": grade_id,
-        "name": name,
-        "subject": subject,
-        "grade": grade,
-        "date": date,
-        "teacher": teacher
+        "id": grade["id"],
+        "name": grade["name"],
+        "subject": grade["subject"],
+        "grade": grade["grade"],
+        "date": grade["date"],
+        "teacher": grade["teacher"]
     }
     res = requests.put(f"{URL}/grades", json=data)
     print(res.json())
@@ -55,13 +68,13 @@ if __name__ == "__main__":
 
     # lambda: get_grades() == get_grades
 
-    bt1 = tk.Button(text="Get", command=lambda: get_grades(), bg="#B0B0B0")
+    bt1 = tk.Button(text="Get", command=lambda: get_grades("forClient/allGrades.json"), bg="#B0B0B0")
     bt1.grid(row=2, column=1)
 
-    bt2 = tk.Button(text="Add", command=get_grades, bg="#B0B0B0")
+    bt2 = tk.Button(text="Add", command=lambda: add_grade("forClient/gradeToAdd.json"), bg="#B0B0B0")
     bt2.grid(row=3, column=1)
 
-    bt3 = tk.Button(text="Delete", command=get_grades, bg="#B0B0B0")
+    bt3 = tk.Button(text="Delete", command=lambda: delete_grade(tId.get(1.0)), bg="#B0B0B0")
     bt3.grid(row=4, column=1)
 
     labId = tk.Label(text="Enter id:", bg="#131417", fg="#A9B7C6")
@@ -70,11 +83,10 @@ if __name__ == "__main__":
     tId = tk.Text(height=1, width=10, bg="#2B2B2B", fg="#A9B7C6")
     tId.grid(row=4, column=3)
 
-    bt4 = tk.Button(text="Put", command=get_grades, bg="#B0B0B0")
+    bt4 = tk.Button(text="Put", command=lambda: change_grade("forClient/gradeToPut.json"), bg="#B0B0B0")
     bt4.grid(row=5, column=1)
-    pass
     # get_grades()
     # add_grade("Misha", "ICT", 12, "2025.11.03", "Alexey")
     # delete_grade(4)
-    #change_grade(1, "Misha", "ICT", 11, "2025.11.05", "Alexey")
+    # change_grade(1, "Misha", "ICT", 11, "2025.11.05", "Alexey")
     window.mainloop()
